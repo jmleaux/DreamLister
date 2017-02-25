273cb7d9ced6482027d397c7c79398b93efcd97f
jmleaux <chip@jmlowe.net>
2017-02-25 14:05:04 +0300

    finished lecture 73...does not build clean

---
+++ Dreamlister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  Dreamlister
+//
+//  Created by J. M. Lowe on 2/25/17.
+//  Copyright © 2017 JMLeaux LLC. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var desc: UILabel!
+}
