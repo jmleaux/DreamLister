d0aca1f7b6b2751d0cdd5e25a29faf8758679049
jmleaux <chip@jmlowe.net>
2017-02-26 18:51:19 +0300

    just added item_extension swift file

---
+++ Dreamlister/Item_extension.swift
@@ -0,0 +1,20 @@
+//
+//  Item_extension.swift
+//  Dreamlister
+//
+//  Created by J. M. Lowe on 2/26/17.
+//  Copyright © 2017 JMLeaux LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Item {
+    
+    // assign date on create
+    public override func awakeFromInsert() {
+        super.awakeFromInsert()
+        self.createdAt = NSDate()
+    }
+    
+}
